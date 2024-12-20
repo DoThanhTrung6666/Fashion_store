2fac0b169874ea60d7371c54b6b0e6d5eeb97ee1
luuhaziki <luudqph41165@fpt.edu.vn>
2024-12-20 23:52:17 +0700

    validate lại banners

--- resources/views/admin/banners/create.blade.php
+++ resources/views/admin/banners/create.blade.php
@@ -6,16 +6,7 @@
             <h1>
                 Thêm mới banner
             </h1>
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
+           
         </section>
 
 
@@ -33,39 +24,63 @@
                                     <label for="exampleInputEmail1">Tiêu đề</label>
                                     <input type="text" class="form-control" placeholder="Tiêu đề..." name="title"
                                         id="title" value="{{ old('title') }}">
+                                        @error('title')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="description">Mô tả</label>
                                     <textarea class="form-control" rows="3" placeholder="Nôị dung..." name="description" id="description">{{ old('description') }}</textarea>
+                                    @error('description')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputFile">Ảnh</label>
                                     <input type="file" name="image_path" id="image">
+                                    @error('image_path')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputEmail1">Đường dẫn</label>
                                     <input type="url" class="form-control" placeholder="Link..." name="link"
                                         id="link" value="{{ old('link') }}">
+                                        @error('link')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputFile">Vị trí</label>
                                     <input type="number" class="form-control" name="position" id="position"
                                         value="{{ old('position', 0) }}">
+                                        @error('position')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="start_date">Ngày bắt đầu</label>
                                     <input type="datetime-local" class="form-control" name="start_date" id="start_date"
                                         value="{{ old('start_date') }}">
+                                        @error('start_date')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="end_date">Ngày kết thúc</label>
                                     <input type="datetime-local" class="form-control" name="end_date" id="end_date"
                                         value="{{ old('end_date') }}">
+                                        @error('end_date')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="is_active" class="form-check-label">Trạng thái hoạt động</label>
                                     <input type="checkbox" name="is_active" id="is_active" value="1"
                                         {{ old('is_active', true) ? 'checked' : '' }}>
+                                        @error('is_active')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
 
                                 </div>
                                 <div class="box-footer">
--- resources/views/admin/banners/update.blade.php
+++ resources/views/admin/banners/update.blade.php
@@ -9,16 +9,7 @@
             <h1>
                 Chỉnh sửa banner
             </h1>
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
+            
         </section>
 
         <section class="content">
@@ -35,14 +26,23 @@
                                     <label for="exampleInputEmail1">Tiêu đề</label>
                                     <input type="text" class="form-control" name="title" id="title"
                                         value="{{ old('title', $banner->title) }}">
+                                        @error('title')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="description">Mô tả</label>
                                     <textarea class="form-control" rows="3" name="description" id="description">{{ old('description', $banner->description) }}</textarea>
+                                    @error('description')
+                                    <span style="color:red">{{ $message }} </span>
+                                @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputFile">Ảnh</label>
                                     <input type="file" name="image_path" id="image">
+                                    @error('image_path')
+                                    <span style="color:red">{{ $message }} </span>
+                                @enderror
                                     <br>
                                     <img src="{{ asset('storage/' . $banner->image_path) }}" alt="{{ $banner->title }}" width="100">
                                 </div>
@@ -51,26 +51,41 @@
                                     <label for="exampleInputEmail1">Đường dẫn</label>
                                     <input type="url" class="form-control" placeholder="Link..." name="link"
                                         id="link" value="{{ old('link', $banner->link) }}">
+                                        @error('link')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputFile">Vị trí</label>
                                     <input type="number" class="form-control" name="position" id="position"
                                         value="{{ old('position', $banner->position, 0) }}">
+                                        @error('position')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="start_date">Ngày bắt đầu</label>
                                     <input type="datetime-local" class="form-control" name="start_date" id="start_date"
                                         value="{{ old('start_date', $banner->start_date) }}">
+                                        @error('start_date')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="end_date">Ngày kết thúc</label>
                                     <input type="datetime-local" class="form-control" name="end_date" id="end_date"
                                         value="{{ old('end_date', $banner->end_date) }}">
+                                        @error('end_date')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="is_active">Trạng thái hoạt động</label>
                                     <input type="checkbox" name="is_active" id="is_active" class="form-check-input"
                                         {{ old('is_active', $banner->is_active) ? 'checked' : '' }}>
+                                        @error('is_active')
+                                        <span style="color:red">{{ $message }} </span>
+                                    @enderror
                                 </div>
                                 
                                 
